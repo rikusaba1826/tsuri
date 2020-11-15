eaa0bef2c2cc449cc7561cc46b4bb65b9ce6431c
rikusaba1826 <kazumasaono0422@gmail.com>
2020-11-16 02:17:04 +0900

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from django.conf import settings #追加   
-from django.conf.urls.static import static #
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
    path('admin/', admin.site.urls),
